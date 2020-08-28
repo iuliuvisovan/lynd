cd80099aaf088b00e2025eda16f82926a28b655c
Iuliu Visovan <iuliuvisovan@usource.ro>
2020-08-28 17:22:43 +0300

    initial commit

---
+++ client/src/components/Header/Logo.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { Link } from 'react-router-dom';
+import { headerItem } from '../shared/helpers';
+
+const Logo = styled(Link)`
+  ${headerItem};
+
+  margin-right: auto;
+  font-size: 24px;
+  font-weight: 500;
+  color: ${props => props.theme.normalText};
+  text-decoration: none;
+
+  @media (max-width: 425px) {
+    padding: 0 8px 0 16px;
+    font-size: 19px;
+  }
+`;
+
+const LogoImgDark = styled.img`
+  height: 32px;
+  margin-left: -8px;
+  display: ${props => props.theme.darkLogoDisplay};
+`;
+
+const LogoImgLight = styled.img`
+  height: 32px;
+  margin-left: -8px;
+  display: ${props => props.theme.lightLogoDisplay};
+`;
+
+export default props => {
+  console.log('props', props);
+
+  return (
+    <Logo to='/'>
+      <LogoImgDark src='/logo.svg' alt='Logo' />
+      <LogoImgLight src='/logo-white.svg' alt='Logo' />
+    </Logo>
+  );
+};
